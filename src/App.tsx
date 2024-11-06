3cb98e34b3951862006f07a9de2ef7017dc980b4
Piotr Kowalski <piotr.98@icloud.com>
2024-11-06 23:23:39 +0100

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css';
 import NavBar from "./navBar.tsx";
+import {Outlet, Route, Routes} from "react-router-dom";
+import PlatformsList from "./components/platforms/platformsList.tsx";
 
 export default function App() {
+    return <Routes>
+        <Route path={"/"} element={<Layout/>}>
+            <Route path={"/platforms"} element={<PlatformsList/>}/>
+        </Route>
+    </Routes>
+}
+
+function Layout() {
     return <>
         <header>
             <NavBar/>
         </header>
+        <main>
+            <div className="container">
+                <Outlet/>
+            </div>
+        </main>
     </>
 }
