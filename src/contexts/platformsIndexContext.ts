25f4b4d04749d312bc9c4d23b12b33972eb45f61
Piotr Kowalski <piotr.98@icloud.com>
2024-11-06 22:38:46 +0100

    Add platforms index context

---
+++ src/contexts/platformsIndexContext.ts
@@ -0,0 +1,26 @@
+import {createContext, useContext} from "react";
+import Platform from "../models/platform.ts";
+import {stateUpdater} from "../utils/types.ts";
+
+export class PlatformsIndexContextHandler {
+    public platforms: Platform[] | undefined;
+    public loading: boolean;
+
+    setPlatformsInternal: stateUpdater<Platform[]>;
+
+    constructor(platforms: Platform[] | undefined, loading: boolean, setPlatforms: stateUpdater<Platform[]>) {
+        this.platforms = platforms;
+        this.loading = loading;
+        this.setPlatformsInternal = setPlatforms;
+    }
+
+    public setPlatforms(editor: (prevState: Platform[]) => Platform[]) {
+        this.setPlatformsInternal(editor);
+    }
+}
+
+export const PlatformsIndexContext = createContext<PlatformsIndexContextHandler>(undefined!);
+
+export default function usePlatformsIndex() {
+    return useContext(PlatformsIndexContext);
+}
