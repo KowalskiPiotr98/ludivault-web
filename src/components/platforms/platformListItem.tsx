d1476becd3007d556eb7bd1db64be3ad6595e0fd
Piotr Kowalski <piotr.98@icloud.com>
2024-11-09 01:05:23 +0100

    Add initial, very simplistic platform listing
    No edits yet, will be done next

--- src/components/platforms/platformListItem.tsx
+++ src/components/platforms/platformListItem.tsx
@@ -1,9 +1,17 @@
 import Platform from "../../models/platform.ts";
+import WrappableText from "../common/text/wrappableText.tsx";
+import ListItem from "../common/lists/listItem.tsx";
+import ListItemBody from "../common/lists/listItemBody.tsx";
 
 class PropTypes {
     item: Platform = undefined!;
 }
 
 export default function PlatformListItem({item}: PropTypes) {
-    return <>{item.name}</>
+    return <ListItem>
+        <ListItemBody>
+            <span className="me-1 text-right min-w-24">({item.shortName})</span>
+            <WrappableText text={item.name}/>
+        </ListItemBody>
+    </ListItem>
 }
