1fba216cc2b9746f50460484762d438c345dd6cd
Piotr Kowalski <piotr.98@icloud.com>
2024-11-09 08:45:37 +0100

    Add utils with action buttons for list items

---
+++ src/components/common/buttons/editButton.tsx
@@ -0,0 +1,14 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPenToSquare} from "@fortawesome/free-solid-svg-icons/faPenToSquare";
+import {Button} from "@headlessui/react";
+
+class PropTypes {
+    onClick: () => void = undefined!;
+    title?: string | undefined;
+}
+
+export default function EditButton({onClick, title = undefined}: PropTypes) {
+    return <Button className="px-1" title={title} onClick={onClick}>
+        <FontAwesomeIcon icon={faPenToSquare}/>
+    </Button>
+}
--- src/components/platforms/platformListItem.tsx
+++ src/components/platforms/platformListItem.tsx
@@ -2,6 +2,9 @@ import Platform from "../../models/platform.ts";
 import WrappableText from "../common/text/wrappableText.tsx";
 import ListItem from "../common/lists/listItem.tsx";
 import ListItemBody from "../common/lists/listItemBody.tsx";
+import ListItemMenu from "../common/lists/listItemMenu.tsx";
+import EditButton from "../common/buttons/editButton.tsx";
+import DeleteButton from "../common/buttons/deleteButton.tsx";
 
 class PropTypes {
     item: Platform = undefined!;
@@ -12,6 +15,10 @@ export default function PlatformListItem({item}: PropTypes) {
         <ListItemBody>
             <span className="me-1 text-right min-w-24">({item.shortName})</span>
             <WrappableText text={item.name}/>
+            <ListItemMenu>
+                <EditButton onClick={() => {}} title={`Edit platform ${item.name}`}/>
+                <DeleteButton onTrigger={() => {}} title={`Delete platform ${item.name}`}/>
+            </ListItemMenu>
         </ListItemBody>
     </ListItem>
 }
