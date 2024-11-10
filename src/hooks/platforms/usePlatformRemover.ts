f374ea7c9a8f8302f37ddd1bd88fb784fbbc3a52
Piotr Kowalski <piotr.98@icloud.com>
2024-11-10 09:47:54 +0100

    Slightly refactor platform remover hook logic
    - return error as string, instead of always undefined
    - fix remover always throwing on success due to lack of short circuit

--- src/hooks/platforms/usePlatformRemover.ts
+++ src/hooks/platforms/usePlatformRemover.ts
@@ -6,20 +6,24 @@ export function usePlatformRemover() {
     const [removing, setRemoving] = useState(false);
     const [error, setError] = useState<string>();
 
-    const remover = async (platform: Platform) => {
+    const remover = async (platform: Platform): Promise<string | undefined> => {
         setRemoving(true);
         setError(undefined);
 
         try {
             const {response} = await deletePlatform(platform);
+            if (response.ok)
+                return undefined;
 
             if (response.status === 404) {
-                setError('Platform not found.');
-                return undefined;
+                const tempError = "Platform not found.";
+                setError(tempError);
+                return tempError;
             }
             if (response.status === 409) {
-                setError('Platform is still in use by other items.');
-                return undefined;
+                const tempError = "Platform is still in use by other items.";
+                setError(tempError);
+                return tempError;
             }
 
             throw "Unexpected response code received when removing a platform";
