1daa5f857127c771d48c4f6debc32239bc2f413d
Piotr Kowalski <piotr.98@icloud.com>
2024-11-16 08:26:11 +0100

    Add `Game` model

---
+++ src/models/game.ts
@@ -0,0 +1,8 @@
+export default class Game {
+    public id: string = "";
+    public platformId: string = "";
+    public title: string = "";
+    public owned: boolean = false;
+    public releaseDate: Date | undefined;
+    public released: boolean = false;
+}
