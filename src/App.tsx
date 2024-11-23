fe43fd1cb0dbc648b798afe6a7e197d61e7a7b4c
Piotr Kowalski <piotr.98@icloud.com>
2024-11-23 10:25:00 +0100

    Add initial, empty games index list

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import NavBar from "./navBar.tsx";
 import {Outlet, Route, Routes} from "react-router-dom";
 import PlatformsList from "./components/platforms/platformsList.tsx";
+import GamesList from "./components/games/gamesList.tsx";
 
 export default function App() {
     return <Routes>
         <Route path={"/"} element={<Layout/>}>
             <Route path={"/platforms"} element={<PlatformsList/>}/>
+            <Route path={"/games"} element={<GamesList/>}/>
         </Route>
     </Routes>
 }
---
+++ src/components/games/gamesList.tsx
@@ -0,0 +1,34 @@
+import useGames from "../../hooks/games/useGames.ts";
+import {useEffect, useState} from "react";
+import PageData, {getOffset} from "../../models/pageData.ts";
+import {GamesIndexContext, GamesIndexContextHandler} from "../../contexts/gamesIndexContext.ts";
+import {PaginationContext, PaginationContextHandler} from "../../contexts/paginationContext.ts";
+import Loader from "../common/loader.tsx";
+import PageSelector from "../common/pageSelector.tsx";
+
+export default function GamesList() {
+    const [pageData, setPageData] = useState<PageData>(new PageData());
+    const {games, setGames, loading} = useGames(getOffset(pageData), pageData.size);
+
+    const contextHandler = new GamesIndexContextHandler(games, setGames);
+    const paginationContext = new PaginationContextHandler(pageData, setPageData);
+
+    useEffect(() => {
+        if (!games)
+            return;
+
+        setPageData(prevState => ({...prevState, isLast: games.length < pageData.size}));
+    }, [games, pageData.size]);
+
+    return <>
+        <GamesIndexContext.Provider value={contextHandler}>
+            {loading ?
+                <Loader/> :
+                <></>
+            }
+        </GamesIndexContext.Provider>
+        <PaginationContext.Provider value={paginationContext}>
+            <PageSelector/>
+        </PaginationContext.Provider>
+    </>
+}
