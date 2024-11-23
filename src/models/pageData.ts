a2dd1143cc112cbedb86bb855132b6f5ac62c92f
Piotr Kowalski <piotr.98@icloud.com>
2024-11-23 10:01:17 +0100

    Add size and offset calc method to page data

--- src/models/pageData.ts
+++ src/models/pageData.ts
@@ -1,4 +1,9 @@
 export default class PageData {
     public current: number = 1;
+    public size: number = 30;
     public isLast: boolean = false;
+
+    public getOffset(): number {
+        return (this.current - 1) * this.size;
+    }
 }
