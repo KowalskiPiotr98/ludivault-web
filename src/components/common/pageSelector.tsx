09f56e3ea32c6c599704e96b3270547286b5680f
Piotr Kowalski <piotr.98@icloud.com>
2024-12-23 17:02:21 +0100

    Improve pagination
    It looks somewhat stupid with the last page constantly changing, but
    there's nothing I can do about this without fiddling with the back-end
    for now.

--- src/components/common/pageSelector.tsx
+++ src/components/common/pageSelector.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@mui/material";
+import {Pagination} from "@mui/material";
 import usePaginationContext from "../../contexts/paginationContext.ts";
 
 export default function PageSelector() {
     const pagination = usePaginationContext();
 
-    //todo: needs styling (and usage of default mui elements), placeholder for now
-    return <div>
-        {pagination.data.current > 1 && <Button onClick={() => pagination.goToPrevious()}>Prev</Button>}
-        {pagination.data.current}
-        {!pagination.data.isLast && <Button onClick={() => pagination.goToNext()}>Next</Button>}
-    </div>
+    return <Pagination
+        sx={{py: 2}}
+        onChange={(_, page) => pagination.goToPage(page)}
+        count={pagination.data.isLast ? pagination.data.current : pagination.data.current + 1}
+    />
 }
