89d4186dca07c3fc99b689cdb2366b34f22f8bc7
Piotr Kowalski <piotr.98@icloud.com>
2024-12-23 13:10:46 +0100

    Add reasonable nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Outlet, Route, Routes} from "react-router";
 import PlatformsList from "./components/platforms/platformsList.tsx";
 import GamesList from "./components/games/gamesList.tsx";
 import GameDetailsRouter from "./components/games/details/gameDetailsRouter.tsx";
+import {Container} from "@mui/material";
 
 export default function App() {
     return <Routes>
@@ -18,11 +19,11 @@ export default function App() {
 
 function Layout() {
     return <>
-        <header>
-            <NavBar/>
-        </header>
+        <NavBar/>
         <main>
-            <Outlet/>
+            <Container maxWidth={"xl"}>
+                <Outlet/>
+            </Container>
         </main>
     </>
 }
--- src/navBar.tsx
+++ src/navBar.tsx
@@ -1,15 +1,17 @@
-import {Link} from "react-router";
+import {AppBar, Box, Button, Container, Toolbar} from "@mui/material";
+import {useNavigate} from "react-router";
 
 export default function NavBar() {
-    return <nav>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/platforms">Platforms</Link>
-                <Link to="/games">Games</Link>
-            </li>
-        </ul>
-    </nav>
+    const navigate = useNavigate();
+
+    return <AppBar position="static">
+        <Container maxWidth={"xl"}>
+            <Toolbar>
+                <Box sx={{flexGrow: 1, display: "flex"}}>
+                    <Button sx={{display: "block", color: "white"}} type={"button"} onClick={() => navigate("/games")}>Games</Button>
+                    <Button sx={{display: "block", color: "white"}} type={"button"} onClick={() => navigate("/platforms")}>Platforms</Button>
+                </Box>
+            </Toolbar>
+        </Container>
+    </AppBar>
 }
