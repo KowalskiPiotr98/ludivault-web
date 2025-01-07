afe4adda8b8c7fdef7d3c767e46d3ac39f746a91
Piotr Kowalski <piotr.98@icloud.com>
2025-01-07 19:25:45 +0100

    Add filter values to games index context

--- src/contexts/gamesIndexContext.ts
+++ src/contexts/gamesIndexContext.ts
@@ -4,8 +4,34 @@ import Game from "../models/game.ts";
 import {ItemIndexContextHandler} from "./itemIndexContext.ts";
 
 export class GamesIndexContextHandler extends ItemIndexContextHandler<Game> {
-    constructor(games: Game[] | undefined, setGames: stateUpdater<Game[] | undefined>) {
+    public readonly titleFilter: string;
+    public readonly setTitleFilter: stateUpdater<string>;
+
+    public readonly ownedFilter: boolean | undefined;
+    public readonly setOwnedFilter: stateUpdater<boolean | undefined>;
+
+    public readonly releasedFilter: boolean | undefined;
+    public readonly setReleasedFilter: stateUpdater<boolean | undefined>;
+
+    public readonly inProgressFilter: boolean | undefined;
+    public readonly setInProgressFilter: stateUpdater<boolean | undefined>;
+
+    constructor(
+        games: Game[] | undefined, setGames: stateUpdater<Game[] | undefined>,
+        titleFilter: string, setTitleFilter: stateUpdater<string>,
+        ownedFilter: boolean | undefined, setOwnedFilter: stateUpdater<boolean | undefined>,
+        releasedFilter: boolean | undefined, setReleasedFilter: stateUpdater<boolean | undefined>,
+        inProgressFilter: boolean | undefined, setInProgressFilter: stateUpdater<boolean | undefined>,
+        ) {
         super(games, setGames);
+        this.titleFilter = titleFilter;
+        this.setTitleFilter= setTitleFilter;
+        this.ownedFilter = ownedFilter;
+        this.setOwnedFilter = setOwnedFilter;
+        this.releasedFilter = releasedFilter;
+        this.setReleasedFilter = setReleasedFilter;
+        this.inProgressFilter = inProgressFilter;
+        this.setInProgressFilter = setInProgressFilter;
     }
 }
 
